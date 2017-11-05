8d0d7675b845a40b1b5994fb49582d335a80f0d4
Skuznetz <s.kuznetzov@mail.ru>
2017-11-05 14:37:01 +0300

    обновим render

--- app/assets/javascripts/components/articles.js
+++ app/assets/javascripts/components/articles.js
@@ -3,12 +3,20 @@
     articles:@props.data
   getDefaultProps: ->
     articles: []
+  
   render: ->
-
-@Articles = React.createClass
-  render: ->
-  React.DOM.div
-    className: 'articles'
-    React.DOM.h2
-      className: 'title'
-      'Articles'
+    React.DOM.div
+      className: 'articles'
+      React.DOM.h2
+        className: 'title'
+        'Articles'
+      React.DOM.table
+        className: 'table table-bordered'
+        React.DOM.thead null,
+          React.DOM.tr null,
+            React.DOM.th null, 'Date'
+            React.DOM.th null, 'Title'
+            React.DOM.th null, 'Amount'
+        React.DOM.tbody null,
+          for article in @state.articles
+            React.createElement Article, key: article.id, article: article
