d33c67f23c3e665f8cd507552617c40f9a4d3828
Skuznetz <s.kuznetzov@mail.ru>
2017-11-05 20:09:12 +0300

    пишем метод create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,19 @@ class ArticlesController < ApplicationController
   def index
   	@articles = Article.all 
   end
+  def create
+  	@article = Article.new(article_params)
+  	if @article.save
+  	  render json: @article
+  	else
+  	  render json: @article.errors,status: :unprocessable_entity
+  	end
+
+end
+    
+  private
+
+  def article_params
+  	params.require(:article).permit(:title,:amount,:date)
+  end
 end
