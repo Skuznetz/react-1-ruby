782e7d653b462aff92ed7d4439bddb33e824cecf
kuznetz <s.kuznetzov@mail.ru>
2018-01-16 19:08:04 +0300

    ставим кавычки

--- app/assets/javascripts/components/article.js
+++ app/assets/javascripts/components/article.js
@@ -3,4 +3,4 @@ render: ->
   React.DOM.tr null,
     React.DOM.td null,@props.article.date 
     React.DOM.td null,@props.article.title
-    React.DOM.td null,amountFormat(@props.article.amount)
+    React.DOM.td null,amountFormat(@props.article.amount);
