925ae648ad5285e8178d02f624af2f5e8645361d
kuznetz <s.kuznetzov@mail.ru>
2018-01-24 21:43:50 +0300

    удалим все файлы

--- app/assets/javascripts/components/article.js
+++
@@ -1,6 +0,0 @@
-@Article = React.createClass
-render: ->
-  React.DOM.tr null,
-    React.DOM.td null,@props.article.date 
-    React.DOM.td null,@props.article.title
-    React.DOM.td null,amountFormat(@props.article.amount);
